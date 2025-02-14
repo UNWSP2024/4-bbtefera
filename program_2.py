579b982c5e9163a71911eddb80b0e9bd4b1c51e8
bbtefera <btefera@students.unwsp.edu>
2025-02-14 09:52:34 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -9,6 +9,15 @@ def main():
     # WRITE YOUR CODE HERE
     ######################
 
+total_tickets= 0
 
-if __name__ == '__main__':
-    main()
+keep_going= 'y'
+
+while keep_going==('y'):
+    movie_name= (input("Enter movie name(or done, if already selected movie):"))
+    tickets= int(input(f"How many tickets would you like?"))
+    keep_going= input('Do you want to see another movie(enter y for yes):')
+# I am a little confused about how to get the tickets to add each other, Specifically the one input.
+total_tickets += tickets
+
+print('The amount of tickets wanted:',total_tickets)
